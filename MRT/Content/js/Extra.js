4e5395fe9fa2915c1b021e530d01d236052df72a
Abdul Aziz Bin Din <azizdin395@gmail.com>
2020-05-18 16:58:25 +0800

    add into github

---
+++ MRT/Content/js/Extra.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+
+    $("#gender > .pdsa-radiobutton").on('click', function () {
+
+        pdsaToggleRadioButtons("#gender", $(this));
+
+    });
+
+});
+
+
+
+function pdsaToggleRadioButtons(groupName, current) {
+
+    // Find currently checked radio button in the group   
+
+    var chk = $(groupName +
+
+        " .pdsa-radiobutton .glyphicon-record");
+
+
+
+    // Remove 'record' glyph
+
+    // Add 'unchecked' glyph
+
+    $(chk).removeClass('glyphicon-record')
+
+        .addClass('glyphicon-unchecked');
+
+
+
+    // Remove the 'unchecked' glyph, add the 'record' glyph
+
+    $(current).find(">:first-child")
+
+        .removeClass('glyphicon-unchecked');
+
+    $(current).find(">:first-child")
+
+        .addClass('glyphicon-record');
+
+}
---
+++ MRT/Models/Route.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MRT.Models
+{
+    public class Route
+    {     
+        [Required(ErrorMessage = "Please select your trip type")]
+        public string tripType { get; set; }
+
+        [Required(ErrorMessage = "Please select your category")]
+        public string LevelCategory { get; set; }
+
+        [Required(ErrorMessage = "Please select your first station")]
+        public string StationFrom { get; set; }
+
+        [Required(ErrorMessage = "Please select your second station")]
+        public string StationTo { get; set; }
+
+        [Required(ErrorMessage = "Please select your number of tickets")]
+        public string NumTicket { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your NRIC or Passport No")]
+        public string idNo { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Email Address")]
+        public string Email { get; set; }
+    }
+}
